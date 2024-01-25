a2f0d8e00eeb7af712568b5f1772879cb1f0bc40
TyroneKong <tyronekong@gmail.com>
2024-01-25 17:54:37 +0000

    wip

--- handlers/register .go
+++ handlers/register .go
@@ -6,12 +6,9 @@ import (
 	"finance/models"
 	"fmt"
 	"log"
-	"os"
-	"strconv"
-	"time"
 
 	"github.com/gofiber/fiber/v3"
-	"github.com/golang-jwt/jwt/v5"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -57,52 +54,4 @@ func HandleRegister(c fiber.Ctx) error {
 	}
 	database.DB.Create(&user)
 	return c.JSON(user)
-}
-
-func HandleLogin(c fiber.Ctx) error {
-	var data map[string]string
-
-	if err := c.Bind().Body(&data); err != nil {
-		return err
-	}
-
-	var user models.User
-
-	database.DB.Where("email = ?", data["email"]).First(&user)
-
-	log.Println("data:", data)
-
-	if err := compareHashedPassword(data, user); err != nil {
-		c.Status(fiber.StatusNotFound)
-		return c.JSON(fiber.Map{
-			"message": "incorrect password",
-		})
-	}
-
-	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"issuer":    strconv.Itoa(int(user.ID)),
-		"expiresAt": time.Now().Add(time.Hour * 24).Unix(),
-	})
-
-	token, err := claims.SignedString([]byte(os.Getenv("API_SECRET")))
-
-	if err != nil {
-		return err
-	}
-
-	cookie := fiber.Cookie{
-		Name:     "jwt",
-		Path:     "/",
-		Value:    token,
-		Expires:  time.Now().Add(time.Hour * 48),
-		HTTPOnly: true,
-	}
-
-	log.Println("cookie", cookie)
-
-	c.Cookie(&cookie)
-
-	return c.JSON(fiber.Map{
-		"message": "success",
-	})
 }
